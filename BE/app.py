76d3be63ec6f1f641152da3d70de0a2da8f924bd
Brandon Chong <54733974+brandoncjh@users.noreply.github.com>
2021-10-09 11:03:35 +0800

    updated new BE routes

--- BE/app.py
+++ BE/app.py
@@ -10,30 +10,63 @@ app = Flask(__name__)
 def hello():
     return "hello world"
 
-@app.route("/submit", methods=['POST'])
-def submit():
-    formData = request.get_json()
-    #extract various fields from form
-    name = formData["name"]
-    field1 = formData["field1"]
-    #and so on...
-    db.employeeData.insert_one({
-        #"userID": userID,
-        "name": name,
-        "field1": field1
-    })
+@app.route("/submitdata", methods=['POST'])
+def submitdata():
+    try:
+        formData = request.get_json()
+        #extract various fields from form
+        name = formData["name"]
+        contact = formData["contact"]
+        email = formData["email"]
+        department = formData["department"]
+        db.employeeData.insert_one({
+            "name": name,
+            "contact": contact,
+            "email": email,
+            "department": department
+        })
+    except:
+        return jsonify({'message': 'error encountered'}), 500
     return jsonify({'message': 'data submitted successfully'}), 200
 
-@app.route("/getscore", methods=['GET'])
-def getscore():
+@app.route("/submitscore", methods=['POST'])
+def submitdata():
+    try:
+        formData = request.get_json()
+        #extract various fields from form
+        name = formData["name"]
+        scores = formData["scores"]
+        #and so on...
+        db.employeeScore.insert_one({
+            "name": name,
+            "scores": scores
+        })
+    except:
+        return jsonify({'message': 'error encountered'}), 500
+    return jsonify({'message': 'data submitted successfully'}), 200
+
+@app.route("/getallscore", methods=['GET'])
+def getallscore():
+    allEmployeeData = db.employeeData.find({}) #return everything
+    allEmployeeScore = db.employeeScore.find({}) #return everything
+    if not result:
+        return jsonify({"message": 'data not found'}), 500
+    else:
+        return jsonify({'allEmployeeData': allEmployeeData, 'allEmployeeScore': allEmployeeScore}), 200    
+
+@app.route("/getscorebyname", methods=['GET'])
+def getscorebyname():
     name = request.args.get('name')
-    result = db.employeeScore.find_one({
+    employeeDataResult = db.employeeData.find_one({
+        'name': name
+    })
+    employeeScoreResult = db.employeeScore.find_one({
         'name': name
     })
     if not result:
         return jsonify({"message": 'data not found'}), 500
     else:
-        return jsonify({'result': result["score"]}), 200
+        return jsonify({'employeeData': employeeDataResult, 'employeeScore': employeeScoreResult}), 200
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
