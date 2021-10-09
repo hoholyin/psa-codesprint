0f872bd289d46bef95643c8cd695a2959652ed63
Brandon Chong <54733974+brandoncjh@users.noreply.github.com>
2021-10-09 11:17:22 +0800

    debug /getallscores cursor issue

--- BE/app.py
+++ BE/app.py
@@ -3,6 +3,8 @@ from flask import Flask, jsonify, request, make_response, url_for
 import os
 import sys
 import pymongo
+import json
+from bson import json_util
 
 app = Flask(__name__)
 
@@ -30,13 +32,12 @@ def submitdata():
     return jsonify({'message': 'data submitted successfully'}), 200
 
 @app.route("/submitscore", methods=['POST'])
-def submitdata():
+def submitscore():
     try:
         formData = request.get_json()
         #extract various fields from form
         name = formData["name"]
         scores = formData["scores"]
-        #and so on...
         db.employeeScore.insert_one({
             "name": name,
             "scores": scores
@@ -47,9 +48,13 @@ def submitdata():
 
 @app.route("/getallscore", methods=['GET'])
 def getallscore():
-    allEmployeeData = db.employeeData.find({}) #return everything
-    allEmployeeScore = db.employeeScore.find({}) #return everything
-    if not result:
+    allEmployeeData = [json.loads(json.dumps(doc, default=json_util.default)) for doc in db.employeeData.find({})] #return everything 
+    allEmployeeScore = [json.loads(json.dumps(doc, default=json_util.default)) for doc in db.employeeScore.find({})] #return everything
+    print("test1")
+    print(allEmployeeData)
+    print("test2")
+    print(allEmployeeScore)
+    if (not allEmployeeData or not allEmployeeScore):
         return jsonify({"message": 'data not found'}), 500
     else:
         return jsonify({'allEmployeeData': allEmployeeData, 'allEmployeeScore': allEmployeeScore}), 200    
@@ -63,7 +68,7 @@ def getscorebyname():
     employeeScoreResult = db.employeeScore.find_one({
         'name': name
     })
-    if not result:
+    if (not employeeDataResult or not employeeScoreResult):
         return jsonify({"message": 'data not found'}), 500
     else:
         return jsonify({'employeeData': employeeDataResult, 'employeeScore': employeeScoreResult}), 200
