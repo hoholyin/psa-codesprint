29be83321ecdae1248bfed3933fa79d95cd15a92
GlennTJR96 <glenn_tjr@hotmail.com>
2021-10-09 16:47:13 +0800

    open dialog box when user click on name

--- FE-psa-codesprint/src/app/app.module.ts
+++ FE-psa-codesprint/src/app/app.module.ts
@@ -7,16 +7,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatDialogModule } from '@angular/material/dialog';
+
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { OverviewPageComponent } from './components/overview-page/overview-page.component';
+import { EmployeeInfoDialogComponent } from './dialogs/employee-info-dialog/employee-info-dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     OverviewPageComponent,
+    EmployeeInfoDialogComponent,
   ],
   imports: [
     BrowserModule,
@@ -25,7 +29,8 @@ import { OverviewPageComponent } from './components/overview-page/overview-page.
     HttpClientModule,
     MatToolbarModule,
     MatTableModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
